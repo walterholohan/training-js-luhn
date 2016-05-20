500ca5e1d9983ae64f1e15c5dbe85a36d262a3b4
walterholohan <walter.holohan@gmail.com>
2016-05-20 16:34:11 +0100

    Luhn Algo jgfjf

--- src/index.js
+++ src/index.js
@@ -6,6 +6,31 @@
  */
 function check(cardNumber) {
     // TODO: implement card number checking
+    var odd = 0,
+    	even = 0,
+    	digit, i;
+
+    //reverse the string
+    cardNumber = cardNumber.split('').reverse().join('');
+
+    for (i=0; i<cardNumber.length; i++) {
+    	digit = parseInt(cardNumber.charAt(i));
+    	//if position of numnber is even
+    	if ((i+1) % 2 == 0) {
+    		digit *= 2
+    		if(digit > 9) {
+    			digit -= 9;
+    			even += digit;
+    		} else {
+    			even += digit;
+    		}
+    	} else {
+    		// IF ODD
+    		odd += digit;
+    	}
+    }
+
+    return (odd + even) % 10 == 0
 }
 
 module.exports = {
